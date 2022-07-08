68d540a335b0faea4bd7b309e8139132a2eb90af
adimasmudi <adimasmudi@gmail.com>
2022-07-08 11:36:43 +0700

    initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div class=" bg-white rounded-lg border border-gray-200 shadow-md  mt-2 mb-4 mx-auto w-2/3 pt-4">
+      <div class="flex flex-row w-40 justify-around items-center">
+        <img
+          class="w-8 h-8 rounded-full"
+          src="https://source.unsplash.com/gySMaocSdqs/600x300"
+          alt="user photo"
+        />
+        <span class="font-bold">Masmudi</span>
+      </div>
+      <div>
+        <a href="#">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-black ml-4 mt-2">
+            Noteworthy technology acquisitions 2021
+          </h5>
+        </a>
+      </div>
+      <a href="#">
+        <img
+          class="rounded-t-lg mx-auto"
+          src="https://source.unsplash.com/gySMaocSdqs/800x400"
+          alt=""
+        />
+      </a>
+      <div class="p-5">
+        <p class="mb-3 font-normal text-gray-700">
+          Here are the biggest enterprise technology acquisitions of 2021 so
+          far, in reverse chronological order.
+        </p>
+      </div>
+      <div className="flex flex-row mb-4 w-1/3 ml-4">
+        <div className="upvote flex flex-row items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-caret-up-fill"
+            viewBox="0 0 16 16"
+            className="text-white stroke-black"
+          >
+            <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
+          </svg>
+          <span className="ml-2">1324</span>
+        </div>
+        <div className="downvote flex flex-row items-center ml-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-caret-down-fill"
+            viewBox="0 0 16 16"
+            className="text-white stroke-black"
+          >
+            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+          </svg>
+          <span className="ml-2">656</span>
+        </div>
+        <div className="comment flex flex-row items-center ml-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-chat"
+            viewBox="0 0 16 16"
+          >
+            <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
+          </svg>
+          <span className="ml-2">3</span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/page/Register.js
+++ src/page/Register.js
@@ -1,23 +1,25 @@
-import './App.css';
+import React from 'react'
 import { Link } from "react-router-dom";
-
-function App() {
+export default function Register() {
   return (
-    <div className="App">
-      <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div class="max-w-md w-full space-y-8">
           <div>
             {/* <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" /> */}
-            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Create your account</h2>
             <p class="mt-2 text-center text-sm text-gray-600">
               Or
-              <Link to="/register" className='font-medium text-indigo-600 hover:text-indigo-500 ml-2'>Register</Link>
+              <Link to="/" className='font-medium text-indigo-600 hover:text-indigo-500 ml-2'>Sign in</Link>
               
             </p>
           </div>
           <form class="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" value="true" />
             <div class="rounded-md shadow-sm -space-y-px">
+            <div>
+                <label for="username" class="sr-only">Username</label>
+                <input id="username" name="username" type="username" autocomplete="username" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+              </div>
               <div>
                 <label for="email-address" class="sr-only">Email address</label>
                 <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
@@ -26,13 +28,13 @@ function App() {
                 <label for="password" class="sr-only">Password</label>
                 <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
               </div>
-            </div>
-
-            <div class="flex items-center justify-between">
-              <div class="text-sm">
-                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </a>
+              <div>
+                <label for="confirm-password" class="sr-only">Confirm Password</label>
+                <input id="confirm-password" name="confirm-password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
               </div>
             </div>
+
+            
 
             <div>
               <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -42,14 +44,11 @@ function App() {
                     <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
                   </svg>
                 </span>
-                Sign in
+                Register
               </button>
             </div>
           </form>
         </div>
       </div>
-    </div>
-  );
+  )
 }
-
-export default App;
